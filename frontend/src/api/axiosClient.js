234c5b023c816274eeaaad8098b0e82452c95a50
AkshatBhole <akshatbhole1@gmail.com>
2025-12-05 00:59:45 +0530

    chnages

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://heartdiseasedetection-1.onrender.com/api',
+    baseURL: 'https://heartdiseasedetection-1.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
